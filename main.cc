b17a2ff31d6d2678272b8f3066d05487105bbd75
Hennadiy Brych <gennadiy.brich@gmail.com>
2023-07-11 17:15:16 -0700

    resurrected old utf8 .manifest plus win utf8 code changes

--- main.cc
+++ main.cc
@@ -1,5 +1,8 @@
 #include <exception>
 #include <format>
+#ifdef _WIN32
+#include <windows.h>
+#endif
 
 import options;
 import redumper;
@@ -15,6 +18,11 @@ using namespace gpsxre;
 int main(int argc, char *argv[])
 {
 	int exit_code = 0;
+	
+#ifdef _WIN32
+	SetConsoleCP(CP_UTF8);
+	SetConsoleOutputCP(CP_UTF8);
+#endif
 
 	try
 	{

10b0a9e56dcaef70d3355884e48e7ad685893f18
Hennadiy Brych <gennadiy.brich@gmail.com>
2025-10-20 22:44:58 -0400

    test macos actions

--- cmake/toolchains/macos-arm64.cmake
+++ cmake/toolchains/macos-arm64.cmake
@@ -6,7 +6,7 @@ set(CMAKE_CXX_COMPILER "${LLVM_ROOT}/bin/clang++")
 set(CMAKE_PREFIX_PATH "${LLVM_ROOT}")
 
 # link libc++ statically
-set(CMAKE_EXE_LINKER_FLAGS_INIT "-nostdlib++ -Wl,-deployment_target_mismatches,suppress ${LLVM_ROOT}/lib/c++/libc++.a ${LLVM_ROOT}/lib/c++/libc++abi.a")
+set(CMAKE_EXE_LINKER_FLAGS_INIT "-nostdlib++ ${LLVM_ROOT}/lib/c++/libc++.a ${LLVM_ROOT}/lib/c++/libc++abi.a")
 
 set(CMAKE_OSX_DEPLOYMENT_TARGET "12.0")
 execute_process(COMMAND xcrun --sdk macosx --show-sdk-path OUTPUT_VARIABLE CMAKE_OSX_SYSROOT OUTPUT_STRIP_TRAILING_WHITESPACE)
--- cmake/toolchains/macos-x64.cmake
+++ cmake/toolchains/macos-x64.cmake
@@ -9,7 +9,7 @@ set(CMAKE_PREFIX_PATH "${LLVM_ROOT}")
 execute_process(COMMAND arch -x86_64 /usr/local/bin/brew --prefix llvm@18 OUTPUT_VARIABLE LLVM_X64_ROOT OUTPUT_STRIP_TRAILING_WHITESPACE)
 
 # link libc++ statically
-set(CMAKE_EXE_LINKER_FLAGS_INIT "-nostdlib++ -Wl,-deployment_target_mismatches,suppress ${LLVM_X64_ROOT}/lib/c++/libc++.a ${LLVM_X64_ROOT}/lib/c++/libc++abi.a")
+set(CMAKE_EXE_LINKER_FLAGS_INIT "-nostdlib++ ${LLVM_X64_ROOT}/lib/c++/libc++.a ${LLVM_X64_ROOT}/lib/c++/libc++abi.a")
 
 set(CMAKE_OSX_DEPLOYMENT_TARGET "12.0")
 execute_process(COMMAND xcrun --sdk macosx --show-sdk-path OUTPUT_VARIABLE CMAKE_OSX_SYSROOT OUTPUT_STRIP_TRAILING_WHITESPACE)

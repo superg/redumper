30c725180bb049999fb9f98408079f3110ea0e2b
Hennadiy Brych <gennadiy.brich@gmail.com>
2025-10-21 17:13:17 -0400

    fix

--- cmake/toolchains/macos-arm64.cmake
+++ cmake/toolchains/macos-arm64.cmake
@@ -5,8 +5,5 @@ set(CMAKE_OSX_ARCHITECTURES "arm64")
 set(CMAKE_CXX_COMPILER "${LLVM_ROOT}/bin/clang++")
 set(CMAKE_PREFIX_PATH "${LLVM_ROOT}")
 
-# link libc++ statically
-set(CMAKE_EXE_LINKER_FLAGS_INIT "-nostdlib++ ${LLVM_ROOT}/lib/c++/libc++.a ${LLVM_ROOT}/lib/c++/libc++abi.a")
-
 set(CMAKE_OSX_DEPLOYMENT_TARGET "12.0")
 execute_process(COMMAND xcrun --sdk macosx --show-sdk-path OUTPUT_VARIABLE CMAKE_OSX_SYSROOT OUTPUT_STRIP_TRAILING_WHITESPACE)

234cbbeb770474457aa119d479556d4e5095c00c
Hennadiy Brych <gennadiy.brich@gmail.com>
2025-10-21 20:35:31 -0400

    hacky way of getting install rpath

--- cmake/toolchains/macos-arm64.cmake
+++ cmake/toolchains/macos-arm64.cmake
@@ -7,3 +7,6 @@ set(CMAKE_PREFIX_PATH "${LLVM_ROOT}")
 
 set(CMAKE_OSX_DEPLOYMENT_TARGET "12.0")
 execute_process(COMMAND xcrun --sdk macosx --show-sdk-path OUTPUT_VARIABLE CMAKE_OSX_SYSROOT OUTPUT_STRIP_TRAILING_WHITESPACE)
+
+# Use LLVM's libc++ instead of system libc++
+set(CMAKE_EXE_LINKER_FLAGS "-L${LLVM_ROOT}/lib/c++")
